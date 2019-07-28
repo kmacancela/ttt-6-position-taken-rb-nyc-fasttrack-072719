4d4b9d7f6302ce1fe071cb3aed8ac8d1d8e046f2
Karina Macancela <karymacancela@gmail.com>
2019-07-28 18:35:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,8 +3,6 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  elsif board[index] != " " || board[index] != "" || board[index] != nil
-    return true
   else 
     return true
   end
